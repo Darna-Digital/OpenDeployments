1b1b47466d001ab6d1f58a270d27dd13f13ca079
Rūtenis Raila <rutenisraila@hotmail.com>
2025-10-12 15:19:07 +0300

    add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
-import { Banner, Head } from "nextra/components";
+import { Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
 import "./globals.css";
@@ -10,10 +10,23 @@ export const metadata: Metadata = {
   title: "OpenDeployments",
 };
 
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
 const navbar = (
   <Navbar
-    logo={<b>Nextra</b>}
+    logo={
+      <div>
+        <img
+          src="/logo/horizontal-dark-foreground.svg"
+          alt="Horizontal dark foreground"
+          className="w-[12rem] dark:hidden"
+        />
+
+        <img
+          src="/logo/horizontal-light-foreground.svg"
+          alt="Horizontal dark foreground"
+          className="w-[12rem] hidden dark:block"
+        />
+      </div>
+    }
     // ... Your additional navbar options
   />
 );
@@ -55,7 +68,6 @@ export default async function RootLayout({
       </Head>
       <body>
         <Layout
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
