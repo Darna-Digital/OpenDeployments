3bc544ab6fdf12de4f9603f3c51647cc2b2a0189
Rūtenis Raila <rutenisraila@hotmail.com>
2025-10-12 16:21:56 +0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div className="x:mx-auto x:flex x:max-w-(--nextra-content-width) x:justify-center x:py-12 x:text-gray-600 x:dark:text-gray-400 x:md:justify-start x:pl-[max(env(safe-area-inset-left),1.5rem)] x:pr-[max(env(safe-area-inset-right),1.5rem)] flex flex-col gap-4 text-sm">
-      hello
+    <div className="x:mx-auto x:flex x:max-w-(--nextra-content-width) x:justify-center x:py-12 x:md:justify-start x:pl-[max(env(safe-area-inset-left),1.5rem)] x:pr-[max(env(safe-area-inset-right),1.5rem)] flex flex-col gap-4 text-sm">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">Own your infra</h1>
+
+        <p>
+          OpenDeployments are an open source collection of guides to help you
+          deploy your application without 3rd party PaaS.
+        </p>
+      </div>
     </div>
   );
 }
