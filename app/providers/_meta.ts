86eef08093cfa5978e67ea1ef0ab23b209ab046d
Rūtenis Raila <rutenisraila@hotmail.com>
2025-10-14 21:37:29 +0300

    add tech-stack-card

---
+++ app/providers/_meta.ts
@@ -0,0 +1,10 @@
+export default {
+  index: {
+    title: "Home",
+    display: "hidden",
+  },
+  about: "About",
+  "get-started": "Get Started",
+  providers: "Providers",
+  nextjs: "Next.js",
+};
