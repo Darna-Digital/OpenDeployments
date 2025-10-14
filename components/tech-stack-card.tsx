e750a174e83471c74a597049e9dc7649e4aabe5f
Rūtenis Raila <rutenisraila@hotmail.com>
2025-10-14 21:55:05 +0300

    wip

--- components/tech-stack-card.tsx
+++ components/tech-stack-card.tsx
@@ -9,7 +9,7 @@ interface Props {
   className?: string;
 }
 
-export default function TechStackICon({ title, href, icon, className }: Props) {
+export default function TechStackCard({ title, href, icon, className }: Props) {
   return (
     <Link
       href={href}
